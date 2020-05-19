521cbcf98c52275b21f3055c918733f27cdfde28
Guy3890 <61104945+Guy3890@users.noreply.github.com>
2020-05-19 18:54:43 +0300

    updating

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/register', {
+    fetch(' https://infinite-cliffs-20622.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
